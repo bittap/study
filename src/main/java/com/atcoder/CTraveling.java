cbaca9b63794fa9fac721063abcbb4d46ce1f4fd
bittap <kinjwo1@naver.com>
2024-05-22 21:16:31 +0900

    Cravelingをコミット ・パリティ

--- src/main/java/com/atcoder/CTraveling.java
+++ src/main/java/com/atcoder/CTraveling.java
@@ -1,51 +1,37 @@
 package com.atcoder;
 
-import java.util.HashSet;
-import java.util.Objects;
 import java.util.Scanner;
-import java.util.Set;
 
 public class CTraveling {
 
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     int N = scan.nextInt();
-    Set<Direction> route = new HashSet<Direction>();
-    route.add(new Direction(0, 0));
-    int[][] destination = new int[N][3];
+    int[] t = new int[N + 1];
+    int[] x = new int[N + 1];
+    int[] y = new int[N + 1];
 
-    for (int i = 0; i < N; i++) {
+    t[0] = x[0] = y[0] = 0;
 
-      for (int j = 0; j < destination[i].length; j++) {
-        destination[i][j] = scan.nextInt();
-      }
+    for (int i = 1; i <= N; i++) {
+      t[i] = scan.nextInt();
+      x[i] = scan.nextInt();
+      y[i] = scan.nextInt();
     }
 
-    int previousStartingPoint = 0;
     boolean can = true;
 
-    for (int i = 0; i < destination.length; i++) {
-      int t = destination[i][0];
-      int x = destination[i][1];
-      int y = destination[i][2];
+    for (int i = 0; i < N; i++) {
+      int j = i + 1;
 
-      route = moveRoute(route, t - previousStartingPoint);
-      Set<Direction> temp = new HashSet<Direction>();
+      int dt = t[j] - t[i];
+      int dx = Math.abs(x[i + 1] - x[i]);
+      int dy = Math.abs(y[i + 1] - y[i]);
 
-      for (Direction Direction : route) {
-        if (Direction.x == x && Direction.y == y) {
-          temp.add(Direction);
-        }
-      }
-
-      route = temp;
-      previousStartingPoint = t;
-
-      if (temp.size() == 0) {
+      if (dt < dx + dy || (dt - dy - dx) % 2 != 0) {
         can = false;
         break;
       }
-
     }
 
     if (can) {
@@ -56,57 +42,6 @@ public class CTraveling {
 
     scan.close();
   }
-
-  private static Set<Direction> moveRoute(Set<Direction> routeParam, int loopCount) {
-    Set<Direction> route = routeParam;
-    for (int i = 0; i < loopCount; i++) {
-      Set<Direction> temp = new HashSet<Direction>();
-
-      for (Direction Direction : route) {
-        int x = Direction.x;
-        int y = Direction.y;
-
-        temp.add(new Direction(x + 1, y));
-        temp.add(new Direction(x - 1, y));
-        temp.add(new Direction(x, y + 1));
-        temp.add(new Direction(x, y - 1));
-      }
-
-      route = temp;
-    }
-
-    return route;
-  }
-
-}
-
-
-class Direction {
-  int x;
-  int y;
-
-  public Direction(int x, int y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(x, y);
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    Direction other = (Direction) obj;
-    return x == other.x && y == other.y;
-  }
-
 
 
 }
